b99887ebf9a8d9a1abc8869994d1b22e6b783eef
DESKTOP-57BGIQA\Asus <shafayat537@gmail.com>
2021-08-29 19:07:11 +0600

    first commit

---
+++ js/sportsDb.js
@@ -0,0 +1,53 @@
+const searchTeam = () => {
+    const serachField = document.getElementById('search-feild');
+    const searchText = serachField.value;
+
+    serachField.value = '';
+    const url = `https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${searchText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearchResult(data.teams))
+}
+searchTeam()
+const displaySearchResult = teams => {
+    // console.log(teams)
+    const serachResult = document.getElementById('search-result')
+    serachResult.textContent = ''
+    teams.forEach(team => {
+        // console.log(team)
+        const div = document.createElement('div');
+        div.classList.add('col')
+        div.innerHTML = `
+             <div onclick="loadTeamDetails(${team.idTeam})" class="card h-100">
+                     <img src="${team.strTeamBadge}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                           <h5 class="card-title">${team.strTeam}</h5>
+                           <p class="card-text">${team.strDescriptionEN.slice(0, 100)}</p>
+                        </div>
+            </div>
+        `;
+        serachResult.appendChild(div);
+    })
+}
+const loadTeamDetails = teamId => {
+    console.log(teamId)
+    const url = `https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=${teamId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTeamDetails(data))
+}
+const displayTeamDetails = team => {
+    console.log(team)
+    const teamDetails = document.getElementById('team-details')
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+         <img src="${team.strTeamBadge}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${team.strTeam}</h5>
+              <p class="card-text">${team.strDescriptionEN}</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+    `
+    teamDetails.appendChild(div);
+}
